a62cf2b43fdd2db83f2da33be046c504af57f505
Jake Grainger <j.p.grainger2@outlook.com>
2022-04-06 09:46:25 +0100

    Update tests for tracesampling.

--- test/tracesampling_test.jl
+++ test/tracesampling_test.jl
@@ -1,5 +1,10 @@
+import HistoricalStormTraceSimulation: StormHistory, TraceSampler
 @testset "tracesampling" begin
     @testset "StormHistory" begin
         @test_throws DimensionMismatch StormHistory(ones(10),ones(9)) # throw error if not same number of summaries as traces
+    end
+
+    @testset "TraceSampler" begin
+        @test_throws DimensionMismatch TraceSampler(Euclidean(),1:10,rand(10),rand(9))        
     end
 end
