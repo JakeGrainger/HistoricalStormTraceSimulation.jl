b3bd3366548ca35169e6661d0b9231696ed3f6ad
Jake Grainger <j.p.grainger2@outlook.com>
2022-04-06 10:14:33 +0100

    Add extra error handling to storm history. Also make types concrete.

--- src/tracesampling.jl
+++ src/tracesampling.jl
@@ -1,8 +1,9 @@
-struct StormHistory{S,T}
-    summaries::Vector{S}
-    traces::Vector{T}
-    function StormHistory(summaries::Vector{S},traces::Vector{T}) where {S,T}
+struct StormHistory
+    summaries::Vector{Vector{Float64}}
+    traces::Vector{Matrix{Float64}}
+    function StormHistory(summaries,traces)
         length(summaries) == length(traces) || throw(DimensionMismatch("Should be equal number of traces and summaries."))
+        all(length(s) == size(t,2) for (s,t) ∈ zip(summaries,traces)) || throw(DimensionMismatch("Each summary should be same length as second dim of corresponding trace."))
         new{S,T}(summaries,traces)
     end
 end
