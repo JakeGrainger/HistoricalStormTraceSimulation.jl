ed44e5d816ed7b67e6eb08b765a6b111845055d7
Jake Grainger <j.p.grainger2@outlook.com>
2022-04-06 12:24:39 +0100

    Add multiple tests and set seed.

--- test/rescalemethods_test.jl
+++ test/rescalemethods_test.jl
@@ -1,5 +1,7 @@
 import HistoricalStormTraceSimulation: rescalesinglevariable!, IdentityRescale, RescaleMaxChangeMin, RescaleMaxPreserveMin, rescaletime
 import Random: randperm
+import Random
+Random.seed!(6266) # seed chosen at random
 @testset "rescalemethods" begin
     @testset "IdentityRescale" begin
         x = randperm(20).*0.1; x_origional = copy(x)
@@ -8,22 +10,26 @@ import Random: randperm
     end
 
     @testset "RescaleMaxChangeMin" begin
-        x = randperm(20).*0.1; xmax = rand()
-        x_origional = copy(x)
-        rescalesinglevariable!(x,xmax,RescaleMaxChangeMin())
-        @test maximum(x) ≈ xmax
-        @test argmax(x) == argmax(x_origional)
+        for _ in 1:10 # test multiple random values
+            x = randperm(20).*0.1; xmax = rand()
+            x_origional = copy(x)
+            rescalesinglevariable!(x,xmax,RescaleMaxChangeMin())
+            @test maximum(x) ≈ xmax
+            @test argmax(x) == argmax(x_origional)
+        end
     end
 
     @testset "RescaleMaxPreserveMin" begin
-        x = randperm(20).*0.1; xmax = rand()
-        x_origional = copy(x)
-        rescalesinglevariable!(x,xmax,RescaleMaxPreserveMin())
-        @test maximum(x) ≈ xmax
-        @test argmax(x) == argmax(x_origional)
-        @test minimum(x) == minimum(x_origional)
-        @test argmax(x) == argmax(x_origional)
-        @test argmin(x) == argmin(x_origional)
+        for _ in 1:10  # test multiple random values
+            x = randperm(20).*0.1; xmax = rand()
+            x_origional = copy(x)
+            rescalesinglevariable!(x,xmax,RescaleMaxPreserveMin())
+            @test maximum(x) ≈ xmax
+            @test argmax(x) == argmax(x_origional)
+            @test minimum(x) == minimum(x_origional)
+            @test argmax(x) == argmax(x_origional)
+            @test argmin(x) == argmin(x_origional)
+        end
     end
     computeduration(x) = x[end]-x[1]
     @testset "rescaletime" begin
