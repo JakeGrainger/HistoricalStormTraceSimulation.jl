1b3a14ab6a3d2e79f5c1382aa996551197b59203
Jake Grainger <j.p.grainger2@outlook.com>
2022-04-06 10:02:47 +0100

    Add more tests.

--- test/tracesampling_test.jl
+++ test/tracesampling_test.jl
@@ -1,4 +1,4 @@
-import HistoricalStormTraceSimulation: StormHistory, TraceSampler
+import HistoricalStormTraceSimulation: StormHistory, TraceSampler, samplehistoricaltrace
 @testset "tracesampling" begin
     @testset "StormHistory" begin
         @test_throws DimensionMismatch StormHistory(ones(10),ones(9)) # throw error if not same number of summaries as traces
@@ -7,5 +7,24 @@ import HistoricalStormTraceSimulation: StormHistory, TraceSampler
     @testset "TraceSampler" begin
         @test_throws DimensionMismatch TraceSampler(Euclidean(),1:10,rand(10),rand(9))        
         @test TraceSampler(Euclidean(),1:50,100) isa TraceSampler
+    end
+
+    @testset "samplesingletrace" begin
+        history = StormHistory([rand(2) for i in 1:100], [rand(2,3) for i in 1:100])
+        summary = rand(2)
+        sampler = TraceSampler(Euclidean(),1:50,100)
+        
+    end
+
+    @testset "samplehistoricaltrace" begin
+        history = StormHistory([rand(2) for i in 1:100], [rand(2,3) for i in 1:100])
+        summary = rand(2)
+        sampler_good = TraceSampler(Euclidean(),1:50,100)
+        sampler_bad = TraceSampler(Euclidean(),101:102,100)
+        @test_throws ErrorException samplehistoricaltrace(summary,history,sampler_bad)
+        trace = samplehistoricaltrace(summary,history,sampler_good)
+        @test trace isa Matrix{Float64}
+        @test size(trace) == (2,3)
+        @test any(t==trace for t in history.traces)
     end
 end
