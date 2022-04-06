62f29ed5b02695640fcc6751bb81537ef12fe406
Jake Grainger <j.p.grainger2@outlook.com>
2022-04-06 10:16:08 +0100

    Update test.

--- test/tracesampling_test.jl
+++ test/tracesampling_test.jl
@@ -1,7 +1,9 @@
 import HistoricalStormTraceSimulation: StormHistory, TraceSampler, samplehistoricaltrace, rescaletrace
 @testset "tracesampling" begin
     @testset "StormHistory" begin
-        @test_throws DimensionMismatch StormHistory(ones(10),ones(9)) # throw error if not same number of summaries as traces
+        @test_throws DimensionMismatch StormHistory([rand(2) for i in 1:3],[rand(2,3) for i in 1:4]) # throw error if not same number of summaries as traces
+        @test_throws DimensionMismatch StormHistory([rand(2) for i in 1:3],[rand(2,3) for i in 1:3])
+        @test StormHistory([rand(2) for i in 1:3],[rand(4,2) for i in 1:3]) isa StormHistory
     end
 
     @testset "TraceSampler" begin
@@ -10,27 +12,27 @@ import HistoricalStormTraceSimulation: StormHistory, TraceSampler, samplehistori
     end
 
     @testset "samplesingletrace" begin
-        history = StormHistory([rand(2) for i in 1:100], [rand(2,3) for i in 1:100])
+        history = StormHistory([rand(2) for i in 1:100], [rand(10,2) for i in 1:100])
         summary = rand(2)
         sampler = TraceSampler(Euclidean(),1:50,100)
         
     end
 
     @testset "samplehistoricaltrace" begin
-        history = StormHistory([rand(2) for i in 1:100], [rand(2,3) for i in 1:100])
+        history = StormHistory([rand(2) for i in 1:100], [rand(10,2) for i in 1:100])
         summary = rand(2)
         sampler_good = TraceSampler(Euclidean(),1:50,100)
         sampler_bad = TraceSampler(Euclidean(),101:102,100)
         @test_throws ErrorException samplehistoricaltrace(summary,history,sampler_bad)
         trace = samplehistoricaltrace(summary,history,sampler_good)
         @test trace isa Matrix{Float64}
-        @test size(trace) == (2,3)
+        @test size(trace) == (10,2)
         @test any(t==trace for t in history.traces)
     end
 
     @testset "rescaletrace" begin
-        trace = rescaletrace(rand(3,10),rand(3),(IdentityRescale(),RescaleMaxChangeMin(),RescaleMaxPreserveMin()))
+        trace = rescaletrace(rand(10,3),rand(3),(IdentityRescale(),RescaleMaxChangeMin(),RescaleMaxPreserveMin()))
         @test trace isa Matrix{Float64}
-        @test size(trace) == (3,10)
+        @test size(trace) == (10,3)
     end
 end
