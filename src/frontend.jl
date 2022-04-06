5a8abf5d0564e4a0780ec3ed40334a9ef88bbd4d
Jake Grainger <j.p.grainger2@outlook.com>
2022-04-06 12:15:16 +0100

    Fix bug with frontend.

--- src/frontend.jl
+++ src/frontend.jl
@@ -8,8 +8,9 @@ Sample new traces given summmaries based on modifications of historical traces.
 - `historical_summaries`: Vector of historical summaries (themselves vectors), assuming final entry is duration.
 - `historical_trace_values`: Vector of historical traces (which are matricies with size(x,2)==length(y) is x is the trace and y the summary).
 - `historical_trace_times`: Vector of trace times, make sure they are all stored as the same type (e.g. a UnitRange).
-- `samplemethod`: Method for sampling from closest points. Passing 1:m will sample uniformly from the closest `m` points.
+- `samplemethod`: Method for sampling from closest points. Passing 1:m will sample uniformly from the closest `m` points. Defaults to 1:50.
 - `rescalemethod`: Tuple of methods for rescaling (one for each column of the trace). Should be a subtype of type RescaleMethod.
+- `summarymetric`: A metric for determining closeness of storm summaries (must be subtype of Metric). Default is Euclidean().
 
 # RescaleMethods
 - `IdentityRescale()`: The identity (no rescale).
@@ -17,11 +18,11 @@ Sample new traces given summmaries based on modifications of historical traces.
 - `RescaleMaxPreserveMin()`: Rescale the maximum to match provided maximum, using linear scaling but preserving the minimum
 """
 
-function sampletraces(new_summaries, historical_summaries, historical_trace_values, historical_trace_times; samplemethod=1:50, rescalemethod)
+function sampletraces(new_summaries, historical_summaries, historical_trace_values, historical_trace_times; samplemethod=1:50, rescalemethod, summarymetric::Metric=Euclidean())
     all(length(s)==length(new_summaries[1]) for s in new_summaries) || throw(ArgumentError("new_summaries should all be the same length."))
     historical_traces = StormTrace.(historical_trace_values,historical_trace_times)
     history = StormHistory(historical_summaries,historical_traces)
-    sampler = TraceSampler(length(historical_summaries),samplemethod)
+    sampler = TraceSampler(summarymetric,samplemethod,length(historical_summaries))
     traces = @showprogress "Sampling historical storms... " [
         sampledsingletrace(s,history,sampler,rescalemethod) for s ∈ new_summaries
     ]
--- test/frontend_test.jl
+++ test/frontend_test.jl
@@ -1,5 +1,14 @@
 @testset "sampletraces" begin
     # generate test data
-    
+    npars = 3
+    Δ = 0.5
+    new_summaries = [[rand(npars); rand(5:20)] for i in 1:10]
+    historical_trace_times = [(1:rand(5:20)).*Δ for i in 1:100]
+    historical_trace_values = [rand(length(t), npars) for t in historical_trace_times]
+    samplemethod = 1:40
+    historical_summaries = [[[maximum(c) for c in eachcol(v)];t[end]-t[1]] for (v,t) in zip(historical_trace_values,historical_trace_times)]
+    rescalemethod = (IdentityRescale(),RescaleMaxChangeMin(),RescaleMaxPreserveMin())
     # tests
+    sample = sampletraces(new_summaries, historical_summaries, historical_trace_values, historical_trace_times; samplemethod=samplemethod, rescalemethod=rescalemethod)
+    @test sample isa Vector{StormTraces}
 end
