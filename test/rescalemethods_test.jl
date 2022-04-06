f87dec985cf84e5a831682bdfc306c4efc44b9b1
Jake Grainger <j.p.grainger2@outlook.com>
2022-04-06 10:04:22 +0100

    Make test vectors unique to make max location tests valid.

--- test/rescalemethods_test.jl
+++ test/rescalemethods_test.jl
@@ -2,13 +2,13 @@ import HistoricalStormTraceSimulation: rescalesinglevariable!, IdentityRescale, 
 
 @testset "rescalemethods" begin
     @testset "IdentityRescale" begin
-        a = rand(10); b = copy(a)
+        a = unique(rand(10)); b = copy(a)
         rescalesinglevariable!(a,rand(),IdentityRescale())
         @test a == b
     end
 
     @testset "RescaleMaxChangeMin" begin
-        x = rand(10); xmax = rand()
+        x = unique(rand(10)); xmax = rand()+0.1
         x_origional = copy(x)
         rescalesinglevariable!(x,xmax,RescaleMaxChangeMin())
         @test maximum(x) == xmax
@@ -16,7 +16,7 @@ import HistoricalStormTraceSimulation: rescalesinglevariable!, IdentityRescale, 
     end
 
     @testset "RescaleMaxPreserveMin" begin
-        x = rand(10); xmax = rand()
+        x = unique(rand(10)); xmax = rand()+0.1
         x_origional = copy(x)
         rescalesinglevariable!(x,xmax,RescaleMaxPreserveMin())
         @test maximum(x) == xmax
