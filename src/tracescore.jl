afa7b61b9f4bce56c319acf775923524c70f2e5d
JakeGrainger <j.p.grainger2@outlook.com>
2022-05-05 13:50:34 +0100

    Finish score function and update constructors.

--- src/tracescore.jl
+++ src/tracescore.jl
@@ -1,15 +1,27 @@
 abstract type TraceScore end
 
-struct MarginalTraceScore{T} <: TraceScore
+struct MarginalTraceScore{T,S} <: TraceScore
     metrics::T
-    function MarginalTraceScore(marginalmetrics::NTuple{N,Metric})
-        new{typeof(marginalmetrics)}(marginalmetrics)
+    weights::S
+    function MarginalTraceScore(metrics::NTuple{N,Metric},weights=ones(size(metrics)))
+        length(metrics) == length(weights) || throw(DimensionMismatch("weights and metrics should be same length."))
+        new{typeof(metrics)}(metrics)
     end
 end
 
 function (mscore::MarginalTraceScore)(t1::StormTrace,t2::StormTrace)
     checkcompatible(t1,t2)
+    checkcompatible(mscore,t1)
+    score = 0.0
+    for i in eachindex(mscore.metrics)
+        @views score += mscore.weights[i]*mscore.metrics[i](t1.value[:,i],t2.value[:,i])
+    end
+    return score
+end
 
+function checkcompatible(m::MarginalTraceScore,t::StormTrace)
+    size(t.value,2) == length(m.metrics) || throw(DimensionMismatch("metrics should be equal to number of cols in trace.value."))
+    nothing
 end
 
 function checkcompatible(t1::StormTrace,t2::StormTrace)
