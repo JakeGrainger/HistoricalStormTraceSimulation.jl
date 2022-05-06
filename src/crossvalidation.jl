7af988bba1e8e3737e727b2ee4ebd7a220e10ef2
JakeGrainger <j.p.grainger2@outlook.com>
2022-05-06 17:00:36 +0100

    Change scoring to not output matrix Also allow optim keywords.

--- src/crossvalidation.jl
+++ src/crossvalidation.jl
@@ -11,12 +11,12 @@ Scores the proposed sampling method by leaving out each trace in turn and predic
 function score_method(history::StormHistory; samplemethod=1:50, rescalemethod, summarymetric::Metric=Euclidean(), tracescore::TraceScore, trace_repeats::Int=1)
     sampler = TraceSampler(summarymetric,samplemethod,length(history))
     temporary_history = deepcopy(history)
-    score = Matrix{Float64}(undef, length(history), trace_repeats)
+    score = 0.0
     for i in 1:length(history)
         temporary_history.summaries[i] .*= Inf # make summary unselectable (this isnt guarenteed to work)
         for j in 1:trace_repeats
             newtrace = samplesingletrace(history.summaries[i],temporary_history,sampler,rescalemethod)
-            score[i,j] = tracescore(newtrace,history.traces[i])
+            score += tracescore(newtrace,history.traces[i])
         end
         temporary_history.summaries[i] = history.summaries[i] # reset summary
     end
@@ -31,12 +31,13 @@ Use optim to find the best distance based on `score_method` scoring.
 - `D` the type of distance (e.g. WeightedEuclidean). Note this is the type, not an instance!
 - `x₀` initial parameters for the distance to start optimisation.
 - `history` the storm history.
+- `optim_kwargs`: Key word arguments to be passed to Optim.
 - `kwargs` additional key word arguments, similar to `score_method`, but not including `summarymetric`, as this is specified by the optimisation.
 """
-function find_best_distance(D::Type{<:Metric},x₀,history; kwargs...)
+function find_best_distance(D::Type{<:Metric},x₀,history; optim_kwargs=(), kwargs...)
     function best_distance_objective(x)
-        sum(score_method(history; summarymetric = D(x), kwargs...))
+        score_method(history; summarymetric = D(x), kwargs...)
     end
-    res = Optim.optimize(best_distance_objective, x₀)
+    res = Optim.optimize(best_distance_objective, x₀, optim_kwargs...)
     return res
 end
