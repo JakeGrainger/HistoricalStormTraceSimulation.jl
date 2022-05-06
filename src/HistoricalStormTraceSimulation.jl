19067a108abba9a7bfd1077aad04aa51d112e4d4
JakeGrainger <j.p.grainger2@outlook.com>
2022-05-06 17:45:38 +0100

    Reexport interpolation methods.

--- src/HistoricalStormTraceSimulation.jl
+++ src/HistoricalStormTraceSimulation.jl
@@ -11,5 +11,6 @@ include("crossvalidation.jl")
 export sampletraces, dataframes2storms
 export RescaleMaxChangeMin, RescaleMean, RescaleIdentity, RescaleMaxPreserveMin
 export score_method, find_best_distance, MarginalTraceScore
+export LinearInterpolation, CubicSplineInterpolation # reexported
 
 end
