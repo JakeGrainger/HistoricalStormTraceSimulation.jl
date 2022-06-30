50f99d18431c66b9a60329e4a9b0bb16868ce2c7
JakeGrainger <j.p.grainger2@outlook.com>
2022-06-30 14:40:32 +0100

    Remove & align tabs.

--- src/rescalemethods.jl
+++ src/rescalemethods.jl
@@ -18,7 +18,7 @@ Linear rescale to adjust the maximum of the new trace to equal the summary value
 Given a trace variable series ``y_j``, and summary value ``x_j``, the new trace 
 ``\\tilde{y}_j`` is constructed using the following rule, ``\\forall t\\in T_y``:
 
-``\\tilde y_j(t) &= \\frac{x_j}{\\max\\limits_{t\\in T_y}y_j(t)}y_j(t).``
+``\\tilde y_j(t) = \\frac{x_j}{\\max\\limits_{t\\in T_y}y_j(t)}y_j(t).``
 
 """
 struct RescaleMaxChangeMin <: RescaleMethod end
@@ -38,7 +38,7 @@ Linear rescale to adjust the maximum of the new trace to equal the summary value
 Given a trace variable series ``y_j``, and summary value ``x_j``, the new trace 
 ``\\tilde{y}_j`` is constructed using the following rule, ``\\forall t\\in T_y``:
 
-``\\tilde y_j(t) &= \\frac{x_j-\\min\\limits_{t\\in T_y}  y_j(t)}{\\max\\limits_{t\\in T_y}y_j(t)-\\min\\limits_{t\\in T_y}  y_j(t)} \\left(y_j(t)-\\min\\limits_{t\\in T_y}  y_j(t)\\right) + \\min\\limits_{t\\in T_y}  y_j(t).``
+``\\tilde y_j(t) = \\frac{x_j-\\min\\limits_{t\\in T_y}  y_j(t)}{\\max\\limits_{t\\in T_y}y_j(t)-\\min\\limits_{t\\in T_y}  y_j(t)} \\left(y_j(t)-\\min\\limits_{t\\in T_y}  y_j(t)\\right) + \\min\\limits_{t\\in T_y}  y_j(t).``
 
 This only works if the new maximum ``x_j`` satisfies ``x_j>\\min\\limits_{t\\in T_y}  y_j(t)``. The formula is ``\\forall t\\in T_y``.
 If this condition is not satisfied, a warning will be displayed.
@@ -61,7 +61,7 @@ Additive rescaling to match the mean of the trace to the summary value.
 
 Given a trace variable series ``y_j``, and summary value ``x_j``, the new trace 
 
-``\\tilde y_j(t) &= y_j(t) - \\overline{y_j} + x_j``
+``\\tilde y_j(t) = y_j(t) - \\overline{y_j} + x_j``
 
 where ``\\overline{y_j} = \\frac{1}{|T_y|} \\sum_{t\\in T_y}y_j(t)``.
 """
@@ -76,7 +76,7 @@ end
 
 Rescale the time to match the duration ``(d)`` and start at 0. The formula is, ``\\forall t \\in T_y``
 
-``\\tilde{t}_t &= (t-\\min T_y) \\frac{d}{\\max T_y - \\min T_y}.``
+``\\tilde{t}_t = (t-\\min T_y) \\frac{d}{\\max T_y - \\min T_y}.``
 
 Then the time vector of ``\\tilde y`` is ``T_{\\tilde{y}} = \\{\\tilde{t}_t \\mid t \\in T_y\\}``.
 """
