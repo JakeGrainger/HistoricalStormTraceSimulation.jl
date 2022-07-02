1bd91393d2175321e715d924059654c53115b0e8
JakeGrainger <j.p.grainger2@outlook.com>
2022-07-02 11:04:09 +0100

    export new rescale method.

--- src/HistoricalStormTraceSimulation.jl
+++ src/HistoricalStormTraceSimulation.jl
@@ -9,7 +9,7 @@ include("tracescore.jl")
 include("crossvalidation.jl")
 
 export sampletraces, dataframes2storms
-export RescaleMaxChangeMin, RescaleMean, RescaleIdentity, RescaleMaxPreserveMin
+export RescaleMaxChangeMin, RescaleMean, RescaleMeanCircularDeg, RescaleIdentity, RescaleMaxPreserveMin
 export expected_score, conditional_expected_score, find_best_distance, MarginalTraceScore
 export StormTrace, StormHistory, nvariables
 export LinearInterpolation, CubicSplineInterpolation # reexported
